a6607ddba3876a352a01eaa0b8d4ccbc3b3326ed
kolbe <water.northground@gmail.com>
2024-01-30 14:44:45 +0900

    revert warning

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Habits365 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: App(),
     );
   }
