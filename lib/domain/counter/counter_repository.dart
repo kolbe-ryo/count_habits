3b9261104186ee8cbbc98db68c7316c35109aacd
kolbe <water.northground@gmail.com>
2024-01-30 23:12:40 +0900

    privider for counter repo

--- lib/domain/counter/counter_repository.dart
+++ lib/domain/counter/counter_repository.dart
@@ -1,4 +1,7 @@
 import 'package:count_habits/domain/counter/entity/counter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counterRepositoryProvider = Provider<CounterRepository>((ref) => throw UnimplementedError());
 
 abstract interface class CounterRepository {
   Future<List<Counter>> fetchAll();
