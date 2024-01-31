769f98e051a65842aed40de311168a281e11f94e
kolbe <water.northground@gmail.com>
2024-01-31 22:14:50 +0900

    change ontap area

--- lib/presentation/pages/detail/components/animated_counter.dart
+++ lib/presentation/pages/detail/components/animated_counter.dart
@@ -14,36 +14,30 @@ class _AnimatedCounterState extends State<AnimatedCounter> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: CustomAnimatedFlipCounter(
-          duration: const Duration(milliseconds: 1500),
-          curve: Curves.bounceOut,
-          textStyle: const TextStyle(
-            fontSize: 200,
-            fontWeight: FontWeight.w400,
-            fontFamily: 'Hachi_Maru_Pop',
-            color: Colors.yellow,
-            shadows: [
-              BoxShadow(
-                color: Colors.red,
-                offset: Offset(4, 4),
-                blurRadius: 10,
-              ),
-            ],
-          ),
-          value: value,
+    return GestureDetector(
+      onTap: () async {
+        value++;
+        setState(() {});
+        await Future<void>.delayed(const Duration(milliseconds: 600));
+        await HapticFeedback.vibrate();
+      },
+      child: CustomAnimatedFlipCounter(
+        duration: const Duration(milliseconds: 1500),
+        curve: Curves.bounceOut,
+        textStyle: const TextStyle(
+          fontSize: 200,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Hachi_Maru_Pop',
+          color: Colors.yellow,
+          shadows: [
+            BoxShadow(
+              color: Colors.red,
+              offset: Offset(4, 4),
+              blurRadius: 10,
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: 'Increment',
-        onPressed: () async {
-          value++;
-          setState(() {});
-          await Future<void>.delayed(const Duration(milliseconds: 600));
-          await HapticFeedback.vibrate();
-        },
-        child: const Icon(Icons.add),
+        value: value,
       ),
     );
   }
