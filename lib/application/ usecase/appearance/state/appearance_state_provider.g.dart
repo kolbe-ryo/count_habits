233968ddc99f45d212cfdb163c88ab4848a7f2cf
kolbe <water.northground@gmail.com>
2024-02-03 14:22:22 +0900

    create appearance state provider

---
+++ lib/application/ usecase/appearance/state/appearance_state_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'appearance_state_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appearanceStateHash() => r'1c42d32fbbe972220dac20a2e79200a255d2676a';
+
+/// See also [AppearanceState].
+@ProviderFor(AppearanceState)
+final appearanceStateProvider =
+    AutoDisposeNotifierProvider<AppearanceState, Appearance>.internal(
+  AppearanceState.new,
+  name: r'appearanceStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$appearanceStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AppearanceState = AutoDisposeNotifier<Appearance>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
