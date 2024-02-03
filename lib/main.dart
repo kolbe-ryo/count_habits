67cf8c77d4c37d117169bd528d4ec1eb52118fc5
kolbe <water.northground@gmail.com>
2024-02-03 13:13:37 +0900

    setting provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'package:count_habits/domain/apprearance/appearance_repository.dart';
+import 'package:count_habits/domain/counter/counter_repository.dart';
+import 'package:count_habits/inflastructure/mock/mock_appearance_repository.dart';
+import 'package:count_habits/inflastructure/mock/mock_counter_repository.dart';
 import 'package:count_habits/presentation/pages/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const Habits365());
-}
-
-class Habits365 extends StatelessWidget {
-  const Habits365({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: App(),
-    );
-  }
+  runApp(
+    ProviderScope(
+      overrides: [
+        appearanceRepositoryProvider.overrideWithValue(MockAppearanceRepository()),
+        counterRepositoryProvider.overrideWithValue(MockCounterRepository()),
+      ],
+      child: const Habits365(),
+    ),
+  );
 }
