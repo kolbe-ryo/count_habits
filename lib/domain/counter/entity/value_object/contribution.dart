20849596b97222487aeceffa1f5fdf8b9dd13422
kolbe <water.northground@gmail.com>
2024-02-06 08:57:51 +0900

    complete contribution test

--- lib/domain/counter/entity/value_object/contribution.dart
+++ lib/domain/counter/entity/value_object/contribution.dart
@@ -15,39 +15,4 @@ class Contribution with _$Contribution {
   factory Contribution.init() => Contribution(contributedAt: [DateTime.now()]);
 
   factory Contribution.fromJson(Map<String, dynamic> json) => _$ContributionFromJson(json);
-
-  /// Use this if user tap today`s counter
-  List<DateTime> checkIn() {
-    contributedAt.add(DateTime.now());
-    return contributedAt;
-  }
-
-  /// 最大連続数を返却するgetter
-  int get maxConsecutiveCount {
-    if (contributedAt.isEmpty) {
-      return 0;
-    }
-
-    // 最大値保持用の変数
-    var maxConsecutiveCount = 1;
-
-    // 一時保存用の変数
-    var currentConsecutiveCount = 1;
-
-    for (var i = 1; i < contributedAt.length; i++) {
-      // 日付が連続している場合
-      if (contributedAt[i].difference(contributedAt[i - 1]).inDays == 1) {
-        currentConsecutiveCount++;
-        continue;
-      }
-      // 日付が連続していない場合
-      // 暫定最大値を超過している場合
-      if (currentConsecutiveCount > maxConsecutiveCount) {
-        maxConsecutiveCount = currentConsecutiveCount;
-      }
-      // 連続カウントをリセットする
-      currentConsecutiveCount = 1;
-    }
-    return currentConsecutiveCount > maxConsecutiveCount ? currentConsecutiveCount : maxConsecutiveCount;
-  }
 }
--- test/domain/counter/contribution_test.dart
+++ test/domain/counter/contribution_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:count_habits/domain/counter/entity/value_object/contribution.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Contribution Objectのinitインスタンスのテスト', () {
+    test('checkIn関数によってDateTimeが1つ存在し、インスタンス作成日と一致すること', () {
+      final contribution = Contribution.init();
+      final datetimeNow = DateTime.now();
+      expect(contribution.contributedAt.length, 1);
+      expect(contribution.contributedAt.first.difference(datetimeNow).inDays == 0, true);
+    });
+  });
+}
