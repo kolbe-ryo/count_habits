de32a590aad3eca6cbdb8829a5677f26f783d469
kolbe <water.northground@gmail.com>
2024-02-05 21:51:31 +0900

    complete counter_value object test

--- test/domain/counter/counter_value_test.dart
+++ test/domain/counter/counter_value_test.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:count_habits/domain/counter/entity/value_object/counter_value.dart';
+import 'package:count_habits/domain/exception/app_exception.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CounterValue Objectのinitインスタンス生成', () {
+    test('任意のnameが設定された場合、任意のnameと0がフィールドに設定されること', () {
+      const name = 'testName';
+      final counterValue = CounterValue.init(name: name);
+      expect(counterValue.name, name);
+      expect(counterValue.count, 0);
+    });
+    test('空のnameが設定された場合、Exceptionがthrowされること', () {
+      const name = '';
+      expect(
+        () {
+          CounterValue.init(name: name);
+        },
+        throwsA(isA<AppException>()),
+      );
+    });
+  });
+}
