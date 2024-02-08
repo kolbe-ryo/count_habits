d249b00fd7322376ffa253fc854d270feb994de9
kolbe <water.northground@gmail.com>
2024-02-08 10:12:43 +0900

    fix update method

--- lib/inflastructure/mock/mock_counter_repository.dart
+++ lib/inflastructure/mock/mock_counter_repository.dart
@@ -1,4 +1,4 @@
-import 'package:count_habits/domain/apprearance/entity/appearance.dart';
+import 'package:collection/collection.dart';
 import 'package:count_habits/domain/counter/counter_repository.dart';
 import 'package:count_habits/domain/counter/entity/counter.dart';
 import 'package:count_habits/domain/counter/entity/value_object/contribution.dart';
@@ -6,14 +6,30 @@ import 'package:count_habits/domain/counter/entity/value_object/counter_value.da
 import 'package:count_habits/domain/exception/app_exception.dart';
 import 'package:count_habits/domain/exception/app_exception_enum.dart';
 
+// テスト用にexceptionフラグを用意しているので通信失敗時のテストの際に利用すること
 class MockCounterRepository implements CounterRepository {
   final List<Counter> _counters = [..._initCounter];
 
   List<Counter> get counters => _counters;
 
-  // テスト用にexceptionフラグを用意しているので通信失敗時のテストの際に利用すること
   @override
-  Future<List<Counter>> fetchAll({bool exception = false}) async {
+  Future<List<Counter>> create(
+    String name, {
+    bool exception = false,
+  }) async {
+    await Future<void>.delayed(const Duration(seconds: 2));
+    if (exception) {
+      throw const AppException(AppExceptionEnum.counterCreate);
+    }
+    final createCounter = Counter.init(name: name);
+    _counters.add(createCounter);
+    return _counters;
+  }
+
+  @override
+  Future<List<Counter>> fetchAll({
+    bool exception = false,
+  }) async {
     await Future<void>.delayed(const Duration(seconds: 2));
     if (exception) {
       throw const AppException(AppExceptionEnum.counterFetchAll);
@@ -31,8 +47,14 @@ class MockCounterRepository implements CounterRepository {
     if (exception) {
       throw const AppException(AppExceptionEnum.counterUpdate);
     }
-    final updatedCounterValue = _counters[int.parse(id)].counterValue.copyWith(name: name);
-    _counters[int.parse(id)] = _counters[int.parse(id)].copyWith(counterValue: updatedCounterValue);
+    final updateCounter = _counters.firstWhereOrNull((element) => element.id == id);
+
+    // 要素が見当たらなかった場合予期せぬエラー
+    if (updateCounter == null) {
+      throw const AppException(AppExceptionEnum.unexpectedException);
+    }
+    final updatedCounterValue = updateCounter.counterValue.copyWith(name: name);
+    _counters[int.parse(id)] = updateCounter.copyWith(counterValue: updatedCounterValue);
     return _counters[int.parse(id)];
   }
 
