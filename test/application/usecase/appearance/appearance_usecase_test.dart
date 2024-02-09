757142b0d150311b8e37e42d47677e60fac88455
kolbe <water.northground@gmail.com>
2024-02-09 09:22:37 +0900

    implement appearance usecase test for fetch

--- test/application/usecase/appearance/appearance_usecase_test.dart
+++ test/application/usecase/appearance/appearance_usecase_test.dart
@@ -1,1 +1,48 @@
-void main() {}
+import 'package:count_habits/application/%20usecase/appearance/appearance_usecase.dart';
+import 'package:count_habits/application/%20usecase/appearance/state/appearance_state_provider.dart';
+import 'package:count_habits/domain/apprearance/appearance_repository.dart';
+import 'package:count_habits/domain/apprearance/entity/appearance.dart';
+import 'package:count_habits/inflastructure/mock/mock_appearance_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('fetchテスト', () {
+    final mockAppearanceRepository = MockAppearanceRepository();
+    final providerContariner = ProviderContainer(
+      overrides: [
+        appearanceRepositoryProvider.overrideWithValue(mockAppearanceRepository),
+      ],
+    );
+    test('取得したAppearanceがstateに格納されていること', () async {
+      await providerContariner.read(appearanceProvider).fetch();
+      final state = providerContariner.read(appearanceStateProvider);
+      expect(state, const Appearance());
+    });
+  });
+
+  group('updateテスト', () {
+    final mockAppearanceRepository = MockAppearanceRepository();
+    final providerContariner = ProviderContainer(
+      overrides: [
+        appearanceRepositoryProvider.overrideWithValue(mockAppearanceRepository),
+      ],
+    );
+    test('更新したAppearanceがstateに格納されていること', () async {
+      await providerContariner.read(appearanceProvider).update(
+            colorid: 1,
+            fontFamily: 'Hachi_Maru_Pop',
+          );
+    });
+  });
+
+  group('resetテスト', () {
+    final mockAppearanceRepository = MockAppearanceRepository();
+    final providerContariner = ProviderContainer(
+      overrides: [
+        appearanceRepositoryProvider.overrideWithValue(mockAppearanceRepository),
+      ],
+    );
+    test('', () async {});
+  });
+}
--- test/application/usecase/counter/counter_usecase_test.dart
+++ test/application/usecase/counter/counter_usecase_test.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'package:count_habits/domain/counter/counter_repository.dart';
+import 'package:count_habits/inflastructure/mock/mock_counter_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('fetchテスト', () {
+    final mockCounterRepository = MockCounterRepository();
+    final providerContariner = ProviderContainer(
+      overrides: [
+        counterRepositoryProvider.overrideWithValue(mockCounterRepository),
+      ],
+    );
+    test('', () async {});
+  });
+}
