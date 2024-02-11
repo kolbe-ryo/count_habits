2ae9285803f4decf64fd41edf2469bea84b070e4
kolbe <water.northground@gmail.com>
2024-02-11 21:37:00 +0900

    resolve state dispose problem

--- test/application/usecase/counter/counter_usecase_test.dart
+++ test/application/usecase/counter/counter_usecase_test.dart
@@ -3,6 +3,8 @@ import 'package:count_habits/application/%20usecase/counter/state/counters_provi
 import 'package:count_habits/domain/counter/counter_repository.dart';
 import 'package:count_habits/domain/counter/entity/counter.dart';
 import 'package:count_habits/inflastructure/mock/mock_counter_repository.dart';
+import 'package:count_habits/util/constants/logger.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test/test.dart';
 
@@ -46,8 +48,35 @@ void main() {
       overrides: [
         counterRepositoryProvider.overrideWithValue(mockCounterRepository),
       ],
-    );
-    test('updateテスト', () async {});
+      // プロバイダを監視して変化を検出する（ここでlitenしないとfetchAllとupdateでstateを共有できない）
+    )..listen<List<Counter>>(
+        countersProvider,
+        (_, __) => logger.i('listen'),
+      );
+
+    test('存在する任意のidに対して任意のnameを渡すとnameが更新されること', () async {
+      const id = '0';
+      const name = 'update';
+
+      await providerContariner.read(counterUsecaseProvider).fetchAll();
+      await providerContariner.read(counterUsecaseProvider).update(
+            id: id,
+            name: name,
+          );
+
+      // 初期値は'number 0'
+      final counter = providerContariner.read(countersProvider.notifier).getCounter(id);
+      expect(counter.counterValue.name, name);
+
+      // このようにすることで登録したlistenerを削除できる（ただし、autoDisposeはされなくなる）
+      // TODO: AutoDisposeの調査を少し行うこと
+      providerContariner
+          .listen<List<Counter>>(
+            countersProvider,
+            (previous, next) => logger.i('listen'),
+          )
+          .close();
+    });
   });
 
   group('deleteテスト', () {
