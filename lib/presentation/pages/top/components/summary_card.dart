b8686d6a119ce1e61a6a26b9e54a944ec9bcfe5a
kolbe <water.northground@gmail.com>
2024-02-17 22:48:53 +0900

    remove ontap from summary card

--- lib/presentation/pages/top/components/summary_card.dart
+++ lib/presentation/pages/top/components/summary_card.dart
@@ -1,4 +1,3 @@
-import 'package:count_habits/presentation/pages/detail/detail_page.dart';
 import 'package:count_habits/presentation/pages/top/components/contribution_tile.dart';
 import 'package:flutter/material.dart';
 
@@ -7,42 +6,33 @@ class SummaryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.of(context).push(DetailPage.route()),
-      child: Card(
-        // color: Colors.blueGrey,
-        elevation: 20,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: const Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Summary',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
+    return Card(
+      // color: Colors.blueGrey,
+      elevation: 20,
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Summary',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Icon(Icons.warning),
-                ],
-              ),
-              SizedBox(height: 10),
-              Text(
-                'You have 3 tasks to complete today',
-                style: TextStyle(
-                  fontSize: 16,
                 ),
-              ),
-              ContributionTile(),
-            ],
-          ),
+                Icon(Icons.warning),
+              ],
+            ),
+            ContributionTile(),
+          ],
         ),
       ),
     );
