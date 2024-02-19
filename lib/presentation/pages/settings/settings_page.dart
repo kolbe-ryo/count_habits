06a0e01f78c95e40c5ffa13aa369b17c1d630b3c
kolbe <water.northground@gmail.com>
2024-02-19 09:03:18 +0900

    add app bar to settings page

--- lib/presentation/pages/settings/settings_page.dart
+++ lib/presentation/pages/settings/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:count_habits/presentation/pages/settings/components/content_card.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -9,12 +10,29 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          // About App
-          SizedBox(),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: Text(
+              'Habit 365',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // About App
+            ContentCard(),
+          ],
+        ),
       ),
     );
   }
