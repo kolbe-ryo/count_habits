6e0d515245a8fa39d17eb28bd5be0ad833c7032c
kolbe <water.northground@gmail.com>
2024-02-20 09:22:26 +0900

    complete switch case

--- lib/presentation/pages/top/components/middle_navigation_items.dart
+++ lib/presentation/pages/top/components/middle_navigation_items.dart
@@ -1,6 +1,7 @@
 import 'package:count_habits/domain/exception/app_exception.dart';
 import 'package:count_habits/domain/exception/app_exception_enum.dart';
 import 'package:count_habits/presentation/pages/top/components/animated_counter.dart';
+import 'package:count_habits/presentation/pages/top/components/edit_counter.dart';
 import 'package:count_habits/presentation/pages/top/state/middle_navigation_bar_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,19 +13,14 @@ class MiddleNavigationItems extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final item = ref.watch(middleNavigationBarStateProvider.select((value) => value.barItemList[index].item));
-
+    final item = ref.watch(
+      middleNavigationBarStateProvider.select((value) => value.barItemList[index].item),
+    );
     switch (item) {
       case 0:
         return const AnimatedCounter();
       case 1:
-        return const Center(
-          child: Text('edit'),
-        );
-      case 2:
-        return const Center(
-          child: Text('delete'),
-        );
+        return const EditCounter();
       default:
         throw const AppException(AppExceptionEnum.unexpectedException);
     }
