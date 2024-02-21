63d2071e93fc5c4365a825661fb850081985aa2a
kolbe <water.northground@gmail.com>
2024-02-21 21:00:45 +0900

    setting color theme

--- lib/presentation/pages/app.dart
+++ lib/presentation/pages/app.dart
@@ -11,19 +11,8 @@ class Habits365 extends StatelessWidget {
     return MaterialApp(
       title: 'Habits365',
       debugShowCheckedModeBanner: false,
-      // TODO: Theme setting
-      theme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-      // theme: ThemeData(
-      //   colorSchemeSeed: Colors.blueGrey,
-      //   useMaterial3: true,
-      //   brightness: Brightness.light,
-      // ),
-      // darkTheme: ThemeData(
-      //   colorSchemeSeed: Colors.green,
-      //   useMaterial3: true,
-      //   brightness: Brightness.dark,
-      // ),
       home: const TopPage(),
       builder: (_, navigator) {
         return Stack(
