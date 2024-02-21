6ea604da4cdb20a11a7309b6e7eb19034d271ed9
kolbe <water.northground@gmail.com>
2024-02-21 20:26:40 +0900

    create delete dialog

--- lib/presentation/components/app_dialog.dart
+++ lib/presentation/components/app_dialog.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-Future<bool?> showDialog(BuildContext context) async {
+Future<bool?> showCustomDialog(BuildContext context) async {
   return showCupertinoDialog<bool?>(
     context: context,
     builder: (context) {
       return CupertinoAlertDialog(
         title: const Text('削除しますか？'),
-        content: const Text('この操作は元に戻せません'),
+        content: const Padding(
+          padding: EdgeInsets.only(top: 16),
+          child: Text('この操作は元に戻せません'),
+        ),
         actions: [
           CupertinoDialogAction(
-            child: const Text('削除'),
+            child: const Text(
+              '削除',
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            ),
             onPressed: () {
-              Navigator.pop(context);
+              Navigator.pop(context, true);
+            },
+          ),
+          CupertinoDialogAction(
+            child: const Text('キャンセル'),
+            onPressed: () {
+              Navigator.pop(context, false);
             },
           ),
         ],
