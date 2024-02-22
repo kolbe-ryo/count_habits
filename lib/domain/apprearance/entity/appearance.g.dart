ec4026e70fdf3b740f72a134c009cf794ebdd387
kolbe <water.northground@gmail.com>
2024-02-22 12:05:19 +0900

    resolve warning

--- lib/domain/apprearance/entity/appearance.g.dart
+++ lib/domain/apprearance/entity/appearance.g.dart
@@ -6,18 +6,15 @@ part of 'appearance.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$AppearanceImpl _$$AppearanceImplFromJson(Map<String, dynamic> json) =>
-    _$AppearanceImpl(
+_$AppearanceImpl _$$AppearanceImplFromJson(Map<String, dynamic> json) => _$AppearanceImpl(
       colorId: json['colorId'] as int? ?? 0,
-      themeMode: $enumDecodeNullable(_$ThemeModeEnumMap, json['themeMode']) ??
-          ThemeMode.system,
+      themeMode: $enumDecodeNullable(_$ThemeModeEnumMap, json['themeMode']) ?? ThemeMode.system,
       fontFamily: json['fontFamily'] as String? ?? 'Monomaniac_One',
     );
 
-Map<String, dynamic> _$$AppearanceImplToJson(_$AppearanceImpl instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$$AppearanceImplToJson(_$AppearanceImpl instance) => <String, dynamic>{
       'colorId': instance.colorId,
-      'themeMode': _$ThemeModeEnumMap[instance.themeMode]!,
+      'themeMode': _$ThemeModeEnumMap[instance.themeMode],
       'fontFamily': instance.fontFamily,
     };
 
