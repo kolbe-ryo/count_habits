d83292065d8ed2a72749ab22677d6ac2ba5a6cfc
kolbe <water.northground@gmail.com>
2024-02-26 20:55:23 +0900

    add safe area

--- lib/presentation/pages/top/top_page.dart
+++ lib/presentation/pages/top/top_page.dart
@@ -1,9 +1,6 @@
-import 'package:count_habits/presentation/pages/settings/settings_page.dart';
 import 'package:count_habits/presentation/pages/top/components/middle_navigation_items.dart';
-import 'package:count_habits/presentation/pages/top/components/middle_navigaton_bar.dart';
 import 'package:count_habits/presentation/pages/top/components/summary_card.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // TODO 全体的にCupertinoDesignを適用すること
@@ -13,51 +10,27 @@ class TopPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('Habit365'),
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: CupertinoColors.systemGrey.withOpacity(0.5),
+        middle: const Text('Habit365'),
       ),
-
-      // AppBar(
-      //   title: const Align(
-      //     alignment: Alignment.centerLeft,
-      //     child: Padding(
-      //       padding: EdgeInsets.only(left: 16),
-      //       child: Text(
-      //         'Habit365',
-      //         style: TextStyle(
-      //           fontWeight: FontWeight.bold,
-      //           fontSize: 30,
-      //         ),
-      //       ),
-      //     ),
-      //   ),
-      //   actions: [
-      //     IconButton(
-      //       icon: const Icon(Icons.settings),
-      //       onPressed: () => Navigator.of(context).push(SettingsPage.route()),
-      //     ),
-      //     const SizedBox(width: 16),
-      //   ],
-      // ),
-      child: PageView.builder(
-        controller: PageController(viewportFraction: 0.9),
-        itemCount: 4,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              const SummaryCard(),
-              const SizedBox(height: 16),
-              // MiddleNavigationBar(index: index),
-              MiddleNavigationItems(index),
-              const SizedBox(height: 100),
-            ],
-          );
-        },
+      child: SafeArea(
+        child: PageView.builder(
+          controller: PageController(viewportFraction: 0.9),
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                const SizedBox(height: 16),
+                const SummaryCard(),
+                const SizedBox(height: 16),
+                MiddleNavigationItems(index),
+                const SizedBox(height: 100),
+              ],
+            );
+          },
+        ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   child: const Icon(Icons.add),
-      //   onPressed: () {},
-      // ),
     );
   }
 }
