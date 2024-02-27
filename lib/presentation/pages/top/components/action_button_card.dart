c2606490726c745ab43ed1fb3a35ef232e542e56
kolbe <water.northground@gmail.com>
2024-02-27 10:06:09 +0900

    change widget

---
+++ lib/presentation/pages/top/components/action_button_card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ActionButtonCard extends StatelessWidget {
+  const ActionButtonCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1,
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CupertinoButton(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.blueGrey,
+              // padding: EdgeInsets.zero,
+              onPressed: () {},
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                // decoration: const BoxDecoration(
+                //   shape: BoxShape.circle,
+                //   color: Color.fromARGB(255, 215, 215, 215),
+                // ),
+                child: const Icon(
+                  CupertinoIcons.info,
+                  size: 40,
+                ),
+              ),
+            ),
+            CupertinoButton(
+              borderRadius: BorderRadius.circular(50),
+              padding: EdgeInsets.zero,
+              onPressed: () {},
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromARGB(255, 215, 215, 215),
+                ),
+                child: const Icon(
+                  CupertinoIcons.multiply,
+                  size: 40,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
