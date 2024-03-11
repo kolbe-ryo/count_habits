f48b7abf04aa086d8cb99c183f1bba152a67bdd3
kolbe <water.northground@gmail.com>
2024-03-11 21:27:15 +0900

    generate assets gen

---
+++ lib/gen/fonts.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: Hachi_Maru_Pop
+  static const String hachiMaruPop = 'Hachi_Maru_Pop';
+
+  /// Font family: Kaisei_Opti
+  static const String kaiseiOpti = 'Kaisei_Opti';
+
+  /// Font family: Monomaniac_One
+  static const String monomaniacOne = 'Monomaniac_One';
+
+  /// Font family: Rampart_One
+  static const String rampartOne = 'Rampart_One';
+
+  /// Font family: Rock_3D
+  static const String rock3D = 'Rock_3D';
+}
