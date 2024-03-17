3f9fea9dfb7f5f961824b299aba0751c94773e22
kolbe <water.northground@gmail.com>
2024-03-17 22:05:25 +0900

    change name

--- lib/presentation/pages/settings/components/text_style_button.dart
+++ lib/presentation/pages/settings/components/text_style_button.dart
@@ -1,5 +1,6 @@
+import 'package:count_habits/application/usecase/appearance/state/appearance_state_provider.dart';
+import 'package:count_habits/presentation/pages/theme/color_schemes.dart';
 import 'package:count_habits/presentation/pages/theme/text_schemes.dart';
-import 'package:count_habits/util/constants/logger.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,14 +14,20 @@ class TextStyleButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(cupertinoThemeProvider);
     return CupertinoButton(
       borderRadius: const BorderRadius.all(Radius.circular(10)),
       // color: theme.barBackgroundColor,
       padding: EdgeInsets.zero,
       child: Text(
         textSchemes[index + 1] ?? '',
+        style: TextStyle(
+          color: theme.primaryColor,
+          fontSize: 22,
+          fontFamily: textSchemes[index + 1],
+        ),
       ),
-      onPressed: () => logger.i('message'),
+      onPressed: () => ref.read(appearanceStateProvider.notifier).setFontFamily(index + 1),
     );
   }
 }
--- lib/presentation/pages/settings/theme_setting_page.dart
+++ lib/presentation/pages/settings/theme_setting_page.dart
@@ -6,52 +6,66 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// TODO レイアウトを調整すること
 class ThemeSettingPage extends ConsumerWidget {
   const ThemeSettingPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(cupertinoThemeProvider);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 16),
-        Text(
-          'Color',
-          style: TextStyle(
-            color: theme.brightness == Brightness.light ? Colors.black : Colors.white,
+    return CupertinoPageScaffold(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          CupertinoSliverNavigationBar(
+            padding: const EdgeInsetsDirectional.only(start: 1),
+            leading: IconButton(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: const Icon(CupertinoIcons.back),
+            ),
+            largeTitle: Text(
+              'Theme',
+              style: theme.textTheme.textStyle,
+            ),
           ),
-        ),
-        const SizedBox(height: 16),
-        GridView.count(
-          shrinkWrap: true,
-          crossAxisCount: 4,
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-          padding: EdgeInsets.zero,
-          children: colorSchemes.entries
-              .map(
-                (e) => ColorStyleButton(
-                  theme: e.value,
-                  index: e.key,
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                'Color',
+                style: theme.textTheme.textStyle.copyWith(
+                  fontSize: 26,
+                  color: theme.brightness == Brightness.light ? Colors.black : Colors.white,
                 ),
-              )
-              .toList(),
-        ),
-        GridView.count(
-          shrinkWrap: true,
-          crossAxisCount: 4,
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-          padding: EdgeInsets.zero,
-          children: colorSchemes.entries
-              .map(
-                (e) => TextStyleButton(index: e.key),
-              )
-              .toList(),
-        ),
-      ],
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverGrid.count(
+              crossAxisCount: 4,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              children: colorSchemes.entries
+                  .map(
+                    (e) => ColorStyleButton(
+                      theme: e.value,
+                      index: e.key,
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          SliverGrid.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+            children: colorSchemes.entries
+                .map(
+                  (e) => TextStyleButton(index: e.key),
+                )
+                .toList(),
+          ),
+        ],
+      ),
     );
   }
 }
