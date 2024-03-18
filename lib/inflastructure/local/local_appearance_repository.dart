53148700feb134e3fab1c3e0458e5bcce0d3b03b
kolbe <water.northground@gmail.com>
2024-03-18 22:50:01 +0900

    create local repos

---
+++ lib/inflastructure/local/local_appearance_repository.dart
@@ -0,0 +1,22 @@
+import 'package:count_habits/domain/apprearance/appearance_repository.dart';
+import 'package:count_habits/domain/apprearance/entity/appearance.dart';
+
+class LocalAppearanceRepository implements AppearanceRepository {
+  @override
+  Future<Appearance> fetch({bool exception = false}) {
+    // TODO: implement fetch
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> reset({bool exception = false}) {
+    // TODO: implement reset
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Appearance> update({int? colorId, int? fontFamilyId, bool exception = false}) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}
