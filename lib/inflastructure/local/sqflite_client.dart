82210a606aaaf707bfae21b1f7def953b911bc53
kolbe <water.northground@gmail.com>
2024-03-24 21:25:30 +0900

    delete comment

--- lib/inflastructure/local/sqflite_client.dart
+++ lib/inflastructure/local/sqflite_client.dart
@@ -22,7 +22,6 @@ class SqfliteClient {
 
   static const appearanceTable = 'Appearance';
 
-  // TODO: FixMe
   Future<void> openDb() async {
     final databasesPath = await getDatabasesPath();
     final path = join(databasesPath, _databaseName);
@@ -40,70 +39,4 @@ class SqfliteClient {
       },
     );
   }
-
-  // Future<List<Todo>> getAllTodos() async {
-  //   if (_database == null) {
-  //     await openDb();
-  //   }
-  //   List<Map<String, dynamic>> todos = await _database
-  //           ?.rawQuery('SELECT * FROM todos ORDER BY date_modified DESC') ??
-  //       [];
-  //   return todos.map((e) => Todo.fromJson(e)).toList();
-  // }
-
-  // Future<Todo?> getTodoById(int id) async {
-  //   if (_database == null) {
-  //     await openDb();
-  //   }
-  //   List<Map<String, dynamic>> todos = await _database
-  //           ?.rawQuery('SELECT * FROM todos WHERE id = ?', [id.toString()]) ??
-  //       [];
-  //   return todos.map((e) => Todo.fromJson(e)).toList().firstOrNull;
-  // }
-
-  // Future<Todo?> createTodo() async {
-  //   if (_database == null) {
-  //     await openDb();
-  //   }
-  //   final id = await _database?.insert('todos', {
-  //     'title': '',
-  //     'content': '',
-  //     'is_complete': 0,
-  //     'date_modified': DateTime.now().toUtc().millisecondsSinceEpoch
-  //   });
-  //   if (id == null) return null;
-  //   return await getTodoById(id);
-  // }
-
-  // Future<int?> updateTodo(int id, String? title, String? content, bool? isComplete) async {
-  //   if (_database == null) {
-  //     await openDb();
-  //   }
-  //   Map<String, dynamic> updatedTodo = {
-  //     'title': title,
-  //     'content': content,
-  //     'is_fav': isComplete == true
-  //         ? 1
-  //         : isComplete == false
-  //             ? 0
-  //             : null,
-  //     'date_modified': DateTime.now().toUtc().millisecondsSinceEpoch
-  //   };
-  //   updatedTodo.removeWhere((_, value) => value == null);
-
-  //   return await _database?.update(
-  //     'todos',
-  //     updatedTodo,
-  //     where: "id = ?",
-  //     whereArgs: [id.toString()],
-  //   );
-  // }
-
-  // Future<int?> deleteTodo(int id) async {
-  //   if (_database == null) {
-  //     await openDb();
-  //   }
-  //   return await _database
-  //       ?.delete('todos', where: "id = ?", whereArgs: [id.toString()]);
-  // }
 }
