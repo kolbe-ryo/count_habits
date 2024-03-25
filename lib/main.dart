cda6dabb13665a2e0a5c71faebd6924943515700
kolbe <water.northground@gmail.com>
2024-03-25 22:31:54 +0900

    override with shared initial value

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:count_habits/domain/apprearance/appearance_repository.dart';
 import 'package:count_habits/domain/counter/counter_repository.dart';
+import 'package:count_habits/inflastructure/local/shared_preferences_client.dart';
 import 'package:count_habits/inflastructure/mock/mock_appearance_repository.dart';
 import 'package:count_habits/inflastructure/mock/mock_counter_repository.dart';
 import 'package:count_habits/presentation/pages/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   runApp(
     ProviderScope(
       overrides: [
         appearanceRepositoryProvider.overrideWithValue(MockAppearanceRepository()),
         counterRepositoryProvider.overrideWithValue(MockCounterRepository()),
+        sharedPreferencesProvider.overrideWithValue(await SharedPreferences.getInstance()),
       ],
       child: const Habits365(),
     ),
