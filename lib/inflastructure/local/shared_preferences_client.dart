985e937628888b664534d40a13f03b4eb64d21f8
kolbe <water.northground@gmail.com>
2024-03-25 22:30:36 +0900

    create shared pref client

---
+++ lib/inflastructure/local/shared_preferences_client.dart
@@ -0,0 +1,4 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((_) => throw UnimplementedError());
