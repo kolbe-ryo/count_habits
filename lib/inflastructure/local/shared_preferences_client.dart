415e7d3e2533e795d9914165a099a0d6104bea10
kolbe <water.northground@gmail.com>
2024-03-26 21:17:15 +0900

    write note for sharedPref

--- lib/inflastructure/local/shared_preferences_client.dart
+++ lib/inflastructure/local/shared_preferences_client.dart
@@ -1,4 +1,5 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// ProviderScopeでoverrideしているので、Unimplementになることはない
 final sharedPreferencesProvider = Provider<SharedPreferences>((_) => throw UnimplementedError());
