cf68eae560bf9f8f2318c16d3a8f3b318444c811
kolbe <water.northground@gmail.com>
2024-03-26 21:22:47 +0900

    create key for counter and appearance

--- lib/inflastructure/local/shared_preferences_client.dart
+++ lib/inflastructure/local/shared_preferences_client.dart
@@ -3,3 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 // ProviderScopeでoverrideしているので、Unimplementになることはない
 final sharedPreferencesProvider = Provider<SharedPreferences>((_) => throw UnimplementedError());
+
+const keyCounter = 'counter';
+
+const keyAppearance = 'appearance';
