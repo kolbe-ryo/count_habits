eee969bf15a40cafe4d3d0894b0a9c170ec9981a
kolbe <water.northground@gmail.com>
2024-03-29 18:59:00 +0900

    create local appearance repository provider

--- lib/inflastructure/local/local_appearance_repository.dart
+++ lib/inflastructure/local/local_appearance_repository.dart
@@ -1,9 +1,22 @@
 import 'package:count_habits/domain/apprearance/appearance_repository.dart';
 import 'package:count_habits/domain/apprearance/entity/appearance.dart';
+import 'package:count_habits/inflastructure/local/shared_preferences_client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// TODO: Providerを追加すること
+final localAppearanceRepositoryProvider = Provider<AppearanceRepository>(
+  (ref) => LocalAppearanceRepository(
+    sharedPreferences: ref.watch(sharedPreferencesProvider),
+  ),
+);
 
 class LocalAppearanceRepository implements AppearanceRepository {
+  LocalAppearanceRepository({
+    required SharedPreferences sharedPreferences,
+  }) : _sharedPreferences = sharedPreferences;
+
+  final SharedPreferences _sharedPreferences;
+
   @override
   Future<Appearance> fetch({bool exception = false}) {
     // TODO: implement fetch
