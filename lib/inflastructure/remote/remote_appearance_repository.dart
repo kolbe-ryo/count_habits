46678f3a76d25c73ae04063631cbd22cc6fc4098
kolbe <water.northground@gmail.com>
2024-03-30 22:00:50 +0900

    remove unnecessary todos

--- lib/inflastructure/remote/remote_appearance_repository.dart
+++ lib/inflastructure/remote/remote_appearance_repository.dart
@@ -4,25 +4,21 @@ import 'package:count_habits/domain/apprearance/entity/appearance.dart';
 class RemoteAppearanceRepository implements AppearanceRepository {
   @override
   Future<Appearance> fetch({bool exception = false}) {
-    // TODO: implement fetch
     throw UnimplementedError();
   }
 
   @override
   Future<void> reset({bool exception = false}) {
-    // TODO: implement reset
     throw UnimplementedError();
   }
 
   @override
   Future<Appearance> update({int? colorId, int? fontFamilyId, bool exception = false}) {
-    // TODO: implement update
     throw UnimplementedError();
   }
 
   @override
   Future<void> create({bool exception = false}) {
-    // TODO: implement create
     throw UnimplementedError();
   }
 }
