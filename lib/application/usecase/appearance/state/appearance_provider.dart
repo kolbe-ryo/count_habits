da6725ac129dd6727b3b8082761baee0c05246ec
kolbe <water.northground@gmail.com>
2024-04-19 21:43:25 +0900

    remove todo

--- lib/application/usecase/appearance/state/appearance_provider.dart
+++ lib/application/usecase/appearance/state/appearance_provider.dart
@@ -4,7 +4,5 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'appearance_provider.g.dart';
 
-// TODO こちらはfetchで全てを取得して保持するだけのProviderで常にデータを唯一の状態源にする（countersProviderみたいにすること）
-
 @Riverpod(keepAlive: true)
 Future<Appearance> appearance(AppearanceRef ref) async => ref.read(appearanceUsecaseProvider).fetch();
