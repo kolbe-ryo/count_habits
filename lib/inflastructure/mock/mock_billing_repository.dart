eaf04af2c69160910022d7b7901a62b756a67325
kolbe <water.northground@gmail.com>
2024-05-08 10:08:01 +0900

    implements billing repo

--- lib/inflastructure/mock/mock_billing_repository.dart
+++ lib/inflastructure/mock/mock_billing_repository.dart
@@ -1,1 +1,36 @@
+import 'package:count_habits/domain/billing/billing_repository.dart';
+import 'package:purchases_flutter/models/customer_info_wrapper.dart';
+import 'package:purchases_flutter/models/offering_wrapper.dart';
+import 'package:purchases_flutter/models/offerings_wrapper.dart';
 
+class MockBillingRepository implements BillingRepository {
+  @override
+  Future<CustomerInfo> customerInfo({bool exception = false}) {
+    // TODO: implement customerInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Offerings> offering({bool exception = false}) {
+    // TODO: implement offering
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CustomerInfo> purchase({required Offering offering, bool exception = false}) {
+    // TODO: implement purchase
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CustomerInfo> restorePurchases({bool exception = false}) {
+    // TODO: implement restorePurchases
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setUp({bool exception = false}) {
+    // TODO: implement setUp
+    throw UnimplementedError();
+  }
+}
