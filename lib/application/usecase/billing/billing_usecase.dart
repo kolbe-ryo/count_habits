70a8277aeb198e6296e2cbd11adc4a3aa5791132
kolbe <water.northground@gmail.com>
2024-05-12 21:38:53 +0900

    add setup and fetch customer info

--- lib/application/usecase/billing/billing_usecase.dart
+++ lib/application/usecase/billing/billing_usecase.dart
@@ -1,8 +1,15 @@
 import 'package:count_habits/util/async_execute_mixin.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:purchases_flutter/object_wrappers.dart';
 
 class BillingUsecase with AsyncExecuteMixin {
   const BillingUsecase(this._ref);
 
   final Ref _ref;
+
+  Future<void> setup() async {}
+
+  Future<CustomerInfo> fetchCustomerInfo() async {
+    return CustomerInfo();
+  }
 }
